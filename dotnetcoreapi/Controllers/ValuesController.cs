dbc12a50ecb077281ab9b684d822ac04895e10b9
unknown <james.c.noonan@gmail.com>
2019-05-06 16:25:33 -0400

    init

---
+++ dotnetcoreapi/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcoreapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "service is alive";
+        }
+        
+    }
+}
